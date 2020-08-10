ea208dc1c1688e103ee4bf41c55a91ca5b1700a3
ClarkTheSup <lknkoqq123@163.com>
2020-08-10 15:23:04 +0800

    finish 2-array

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,6 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const person = collection.find(e => e.age <= 20 && e.age >= 11);
+  console.log(person);
+  return person.name;
 }
