72515b5a33858520db801443342e8c81e3dabf95
ClarkTheSup <lknkoqq123@163.com>
2020-08-10 16:16:53 +0800

    finish 4-class

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,17 @@
+import Person from './person';
 // TODO 20: 在这里写实现代码
+class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    if (this.klass) {
+      return `${super.introduce()} I am a Teacher. I teach Class ${this.klass}.`;
+    }
+    return `${super.introduce()} I am a Teacher. I teach No Class.`;
+  }
+}
+
+export default Teacher;
